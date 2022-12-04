3890322c9f2cff799e86dc11ee12ef41cfa7cf7d
radalssemi <114589530+radalssemi@users.noreply.github.com>
2022-12-04 13:30:20 +0100

    rotatyboi

---
+++ postsscript.js
@@ -0,0 +1,48 @@
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+
+
+
+
+
+for (var i = 0; i < 4 ; i++) {
+  document.querySelectorAll(".postDownload")[i].addEventListener("mouseover", function() {
+    randomRotate(this, true); // it is checked if input #2 is true to see if it's the download button to have a larger range for random number. it looks kinda bad when the images rotate a lot but it also looks kinda bad when the download button doesn't rotate enough
+  });
+}
+for (var i = 0; i < 4 ; i++) {
+  document.querySelectorAll(".postDownload")[i].addEventListener("mouseout", function() {
+    unRotate(this);
+  });
+}
+
+const images = document.querySelectorAll(".images img").length;
+for (var i = 0; i < images ; i++) {
+  document.querySelectorAll("div.images img")[i].addEventListener("mouseover", function() {
+    randomRotate(this);
+  });
+}
+for (var i = 0; i < images ; i++) {
+  document.querySelectorAll("div.images img")[i].addEventListener("mouseout", function() {
+    unRotate(this);
+  });
+}
+
+
+
+
+
+
+function randomRotate(element, isDownloadButton) {
+  if(isDownloadButton == true) {
+    element.style.rotate = randomIntFromInterval(-2, 2) + "deg";
+  }
+  else {
+    element.style.rotate = randomIntFromInterval(-1, 1) + "deg";
+  }
+}
+function unRotate(element) {
+  element.style.rotate = "0deg";
+}
