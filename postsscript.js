ed6338aaa726ebf699d301e6c8a6823b581d2859
radalssemi <114589530+radalssemi@users.noreply.github.com>
2022-12-04 13:35:02 +0100

    commenting

--- postsscript.js
+++ postsscript.js
@@ -1,4 +1,4 @@
-function randomIntFromInterval(min, max) { // min and max included 
+function randomIntFromInterval(min, max) { // radom int between two numbers
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
@@ -6,7 +6,8 @@ function randomIntFromInterval(min, max) { // min and max included
 
 
 
-
+// these for loops add event listeners on all download buttons and images to call function to apply a random rotation
+// const images = document.querySelectorAll(".images img").length;
 for (var i = 0; i < 4 ; i++) {
   document.querySelectorAll(".postDownload")[i].addEventListener("mouseover", function() {
     randomRotate(this, true); // it is checked if input #2 is true to see if it's the download button to have a larger range for random number. it looks kinda bad when the images rotate a lot but it also looks kinda bad when the download button doesn't rotate enough
@@ -34,7 +35,7 @@ for (var i = 0; i < images ; i++) {
 
 
 
-
+//hover effect for random rotation
 function randomRotate(element, isDownloadButton) {
   if(isDownloadButton == true) {
     element.style.rotate = randomIntFromInterval(-2, 2) + "deg";
