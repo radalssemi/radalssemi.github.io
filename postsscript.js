a036c9827bd55d528451650a0bdf59df7e95a118
radalssemi <114589530+radalssemi@users.noreply.github.com>
2022-12-04 13:37:11 +0100

    number of download buttons no longer fixed value

--- postsscript.js
+++ postsscript.js
@@ -7,7 +7,7 @@ function randomIntFromInterval(min, max) { // radom int between two numbers
 
 
 // these for loops add event listeners on all download buttons and images to call function to apply a random rotation
-// const images = document.querySelectorAll(".images img").length;
+const downloadButtonCount = document.querySelectorAll(".postDownload").length;
 for (var i = 0; i < 4 ; i++) {
   document.querySelectorAll(".postDownload")[i].addEventListener("mouseover", function() {
     randomRotate(this, true); // it is checked if input #2 is true to see if it's the download button to have a larger range for random number. it looks kinda bad when the images rotate a lot but it also looks kinda bad when the download button doesn't rotate enough
@@ -19,13 +19,13 @@ for (var i = 0; i < 4 ; i++) {
   });
 }
 
-const images = document.querySelectorAll(".images img").length;
-for (var i = 0; i < images ; i++) {
+const imagesCount = document.querySelectorAll(".images img").length;
+for (var i = 0; i < imagesCount ; i++) {
   document.querySelectorAll("div.images img")[i].addEventListener("mouseover", function() {
     randomRotate(this);
   });
 }
-for (var i = 0; i < images ; i++) {
+for (var i = 0; i < imagesCount ; i++) {
   document.querySelectorAll("div.images img")[i].addEventListener("mouseout", function() {
     unRotate(this);
   });
